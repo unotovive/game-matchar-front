e7589436fd8213b73efa3896bf1d03b90c0b2e36
unotovive <tec.unotovive@gmail.com>
2019-03-08 16:51:06 +0900

    Fixes 環境構築

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
-
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
