ccdbc03d533ce79f29395b5440664b2cf47ad1b3
unotovive <tec.unotovive@gmail.com>
2019-03-13 17:21:25 +0900

    Fixes TabUI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
+  <div class="home">
+    <tab :selected="0"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
-@Component({})
-export default class Home extends Vue {
-}
+import Tab from '@/component/Tab.vue';
+
+@Component({
+  components: { Tab },
+})
+export default class Home extends Vue {}
 </script>
 
 <style lang="scss" scoped>
